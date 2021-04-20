19aefc8144a4132159d0ba0c4d3f92d94fefe52f
Rodolfo Beccari de Oliveira <rod.beccari@gmail.com>
2021-04-20 00:49:07 -0300

    chore(swagger): adding minimal Swagger configuration

--- apps/aletheia-api/src/main.ts
+++ apps/aletheia-api/src/main.ts
@@ -3,8 +3,9 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,10 +13,23 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  configureSwagger(app);
+
   const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
+
+  await app.listen(port);
+}
+
+function configureSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Aletheia API')
+    .setDescription('API Restful ')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 }
 
 bootstrap();
